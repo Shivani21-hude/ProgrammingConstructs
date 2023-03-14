58077093c1e649ac0e88501121b76c399c96bc5c
Shivani21-hude <shivani21hude@gmail.com>
2023-03-14 20:27:22 +0530

    [Shivani]-If and Else statement problem

---
+++ ProgrammingConstucts/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ProgrammingConstucts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" **** Welcome to Programming Constructs Problems **** ");
+
+            IfElseStatement statement = new IfElseStatement();
+
+            Console.WriteLine("==> Check Whether two integer are equal or not ");
+            statement.CheckIntegervalueEqualOrNot();
+
+            Console.WriteLine("\n==> To Check the given number is even or odd " );
+            statement.CheckNumberEvenOrOdd();
+
+            Console.WriteLine("\n==> To check whether the person is eligible or not for voting");
+            statement.EligibleForVotes();
+        }
+    }
+}
